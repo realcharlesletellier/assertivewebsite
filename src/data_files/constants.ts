28c9b4d9d5ed9cc4c53138804fd1f8aea7c94bca
Charles <realcharlesletellier@gmail.com>
2024-12-11 18:44:35 -0500

    Fix quality logo

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,4 +1,4 @@
-import ogImageSrc from "@images/assertive-ai-logo (8).png";
+import ogImageSrc from "@images/assertive-ai-logo.png";
 
 export const SITE = {
   title: "Assertive - Autonomous Unit Test Generation with AI",
